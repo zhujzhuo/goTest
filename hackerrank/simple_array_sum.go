7f0ee6d454925b8233239a3bdd5111f089768bcd
zhujzhuo <zhujzhuo@msn.com>
2019-12-18 08:05:37 +0800

    hackerrank

---
+++ hackerrank/simple_array_sum.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+    "fmt"
+    "strconv"
+    "os"
+)
+
+/*
+ * Complete the simpleArraySum function below.
+ */
+func simpleArraySum(ar []int32) int32 {
+    /*
+     * Write your code here.
+     */
+    var sum int32
+    n := len(ar)
+    for i:=0;i<n;i++{
+        sum += ar[i]
+    }
+    return sum
+}
+
+func main() {
+    arCount := 5
+    arTemp  := [] string{"1", "2", "3", "4", "5"}
+    var ar []int32
+
+    for arItr := 0; arItr < int(arCount); arItr++ {
+        arItemTemp, err := strconv.ParseInt(arTemp[arItr], 10, 64)
+        checkError(err)
+        arItem := int32(arItemTemp)
+        ar = append(ar, arItem)
+    }
+
+    result := simpleArraySum(ar)
+
+    fmt.Fprintf(os.Stdout, "%d\n", result)
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
+
+/*
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+func simpleArraySum(ar []int32) int32 {
+    var sum int32
+    n := len(ar)
+    for i:=0;i<n;i++{
+        sum += ar[i]
+    }
+    return sum
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 1024 * 1024)
+
+    arCount, err := strconv.ParseInt(readLine(reader), 10, 64)
+    checkError(err)
+
+    arTemp := strings.Split(readLine(reader), " ")
+
+    var ar []int32
+
+    for arItr := 0; arItr < int(arCount); arItr++ {
+        arItemTemp, err := strconv.ParseInt(arTemp[arItr], 10, 64)
+        checkError(err)
+        arItem := int32(arItemTemp)
+        ar = append(ar, arItem)
+    }
+
+    result := simpleArraySum(ar)
+
+    fmt.Fprintf(writer, "%d\n", result)
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
+*/
